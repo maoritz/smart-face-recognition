b22b507b8d717b389c64027160c0cfc5672b3eb1
maoritz <maorattias17@gmail.com>
2020-03-08 19:39:55 +0200

    Updates

---
+++ precache-manifest.8670ea65b25c1da05d2b598985ab537c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a58da7b0ef44738dfd92122c4c87bf5",
+    "url": "/smart-face-recognition/index.html"
+  },
+  {
+    "revision": "5661f40d80e58a8b0feb",
+    "url": "/smart-face-recognition/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66e64ff25e33b643f86b",
+    "url": "/smart-face-recognition/static/js/2.3d31ddc4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/smart-face-recognition/static/js/2.3d31ddc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5661f40d80e58a8b0feb",
+    "url": "/smart-face-recognition/static/js/main.52c466c9.chunk.js"
+  },
+  {
+    "revision": "66d7c8bbc1bb247d7fec",
+    "url": "/smart-face-recognition/static/js/runtime-main.2d69a1d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-face-recognition/static/media/logo.5d5d9eef.svg"
+  }
+]);
