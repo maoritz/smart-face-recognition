6dffa5318d137b45d7822fe88c7b291bd2dc1fc9
maoritz <maorattias17@gmail.com>
2020-03-09 22:46:51 +0200

    Face detection box

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import "./faceRecognition.css";
 
 const FaceRecognition = props => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img alt="image" src={props.imageUrl} width="500px" heigh="auto" />
+        <img
+          id="inputimage"
+          alt="image"
+          src={props.imageUrl}
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: props.box.topRow,
+            right: props.box.rightCol,
+            bottom: props.box.bottomRow,
+            left: props.box.leftCol
+          }}
+        ></div>
       </div>
     </div>
   );
